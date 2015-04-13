b1b777653f7b1fa5ed45f045f323aa3f7af8ec15
bshap27 <rshapiro3@gmail.com>
2015-04-13 16:07:34 -0400

    removed resources search func

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,6 @@
 class SearchController < ApplicationController
   before_action :authenticate_user!
 
-
   def show
     @search_term = params["search"]["search_term"].downcase
     search_results
@@ -10,13 +9,7 @@ class SearchController < ApplicationController
 private
 
   def search_results
-    category = params["category"]
-    if category == "Discussion" || category == "None"
-      @post_results = Post.where("content like '%#{@search_term}%'")
-    end
-    if category == "Resource" || category == "None"
-      @resource_results = Resource.where("description like '%#{@search_term}%'")
-    end
+    @post_results = Post.where("content like '%#{@search_term}%'")
   end
 
 end
