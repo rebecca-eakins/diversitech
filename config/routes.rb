d14525de149c39efcbd6c4b8c43a9539d6901295
rebecca-eakins <rebecca.a.eakins@gmail.com>
2015-04-14 09:21:18 -0400

    create about page/route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   root 'welcome#index'
+  get '/about' => 'welcome#about'
   get 'search_results' => 'search#show'
 
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks", :registrations => "users/registrations" }
