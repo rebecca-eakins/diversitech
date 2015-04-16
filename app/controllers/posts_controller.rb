eae81baac4b4ec53f373ace16508b99efbc602e8
bshap27 <rshapiro3@gmail.com>
2015-04-16 11:12:01 -0400

    added stubbed routes to post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,34 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!
+  before_action :find_post, :only => [:show, :edit, :update, :destroy]
 
   def index
     #redirect_to tab self-selected by user as primary interest.
   end
 
   def show
-    @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+
+  private
+
+  def find_post
+    Post.find(params[:id])
   end
  
 end
