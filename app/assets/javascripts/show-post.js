fece285962c8f7ca522284442805cb2bff8af39f
bshap27 <rshapiro3@gmail.com>
2015-04-17 15:19:31 -0400

    child post edit page only allows user to edit content

--- app/assets/javascripts/show-post.js
+++ app/assets/javascripts/show-post.js
@@ -3,7 +3,7 @@ $(function(){
 	$("a.new-comment-link").click(function(e) {
 		e.preventDefault();
 		var parentPostID = $(this).data().id;
-		$("#new-comment-for-" + parentPostID).toggleClass("hide");
+		$("#comment-for-" + parentPostID).toggleClass("hide");
 	});
 
 });
